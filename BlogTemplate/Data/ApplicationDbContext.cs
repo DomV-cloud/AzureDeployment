c46e5788a4028a18855a2223b1125e2633900631
Petr Barabáš <barabas@spsejecna.cz>
2023-02-25 11:33:35 +0100

    Add project files.

---
+++ BlogTemplate/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogTemplate.Models;
+
+namespace BlogTemplate.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BlogTemplate.Models.Article> Article { get; set; }
+    }
+}
